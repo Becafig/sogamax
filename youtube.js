de305e5210e2ea9fae7249ec0c8c53dbdca48910
Becafig <lc.becafig@gmail.com>
2024-12-16 00:54:32 -0300

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const placeholders = document.querySelectorAll('.youtube-placeholder');
+
+    placeholders.forEach(placeholder => {
+        placeholder.addEventListener('click', function () {
+            const videoId = placeholder.getAttribute('data-video-id');
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+            iframe.setAttribute('frameborder', '0');
+            iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+            iframe.setAttribute('allowfullscreen', true);
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+
+            placeholder.innerHTML = ''; // Remove o placeholder
+            placeholder.appendChild(iframe); // Adiciona o iframe
+        });
+    });
+});
